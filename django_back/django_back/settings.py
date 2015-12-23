b09f969dccee4440c751e0d0c6831b199d0f78a9
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-12-23 18:56:52 +0800

    minor

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'link_api',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'ydh0114',
         'HOST': 'localhost',
         'PORT': '3306',
     }
