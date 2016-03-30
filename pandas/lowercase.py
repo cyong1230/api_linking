23e69a93f4a5a2f01c85eec8c1dd24c1d80f2ea7
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2016-03-30 09:58:44 +0800

    lowercase.py

--- pandas/lowercase.py
+++ pandas/lowercase.py
@@ -1,5 +1,6 @@
-f = open('pandas_q_a_c.lc', 'w')
-with open('pandas_q_a_c.txt', 'r') as fileinput:
+import sys
+f = open(sys.argv[2], 'w')
+with open(sys.argv[1], 'r') as fileinput:
    for line in fileinput:
        line = line.lower()
        f.write(line)
